b138a337f8c7a372a02f61002b8e9a9d000f3bc6
M@W <mehdicr7wahid@gmail.com>
2022-10-23 14:00:00 +0430

    added the admin page

---
+++ AMS_Frontend/attendance/components/adminPageComponent.js
@@ -0,0 +1,8 @@
+
+const AdminPage = () => {
+  return (
+    <div>Admin</div>
+  )
+}
+
+export default AdminPage
---
+++ AMS_Frontend/attendance/pages/admin.js
@@ -0,0 +1,8 @@
+
+const Admin = () => {
+  return (
+    <Admin/>
+  )
+}
+
+export default Admin
